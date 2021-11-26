e52052717dce546a18fd7ee71933b8db8be4b014
Barnaby Shearer <b@zi.is>
2021-11-26 12:22:03 +0000

    Add docs/conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Sphinx config."""
+
+extensions = ["sphinxcontrib.autoprogram", "sphinx.ext.autodoc"]
+
+project = "Ergonomic utilities for the terraform CLI."
+copyright = "2021, Barnaby Shearer"
+author = "Barnaby Shearer"
+
+master_doc = "index"
